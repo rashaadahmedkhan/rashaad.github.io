d630f4be76d5b265b5d087fc0a3887b02c9450e8
Rashaad <54183706+rashaad-ak@users.noreply.github.com>
2020-10-19 22:05:04 +0530

    Add files via upload

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#0067A5',
+    lineColor: '#C0C0C0'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
